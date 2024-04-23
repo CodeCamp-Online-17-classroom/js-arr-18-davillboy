a0f2c752093d1d5b4696549f01ee3e95f913ea35
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 20:00:53 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // write code here
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+let sumKeys = Object.keys(salaries).reduce((sum, key) => sum + salaries[key], 0);
+console.log("ผลรวมโดยใช้ Object.keys:", sumKeys);
+
+let sumValues = Object.values(salaries).reduce((sum, value) => sum + value, 0);
+console.log("ผลรวมโดยใช้ Object.values:", sumValues);
+
+let sumEntries = Object.entries(salaries).reduce((sum, [key, value]) => sum + value, 0);
+console.log("ผลรวมโดยใช้ Object.entries:", sumEntries);
